a6e490c5ac4509fb9427791683bf16592bb04748
kryptondv <kryptondevpl@gmail.com>
2021-01-06 11:46:44 +0100

    add main-layout component and notes-list component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainLayoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
